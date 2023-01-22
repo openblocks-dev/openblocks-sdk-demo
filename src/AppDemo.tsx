afda6e11be7d60bf50fe9077d8cc5f47dc43614d
sarike <sarike@timefly.cn>
2023-01-22 17:30:54 +0800

    fix base url

--- src/AppDemo.tsx
+++ src/AppDemo.tsx
@@ -3,7 +3,6 @@ import { OpenblocksAppView } from "openblocks-sdk";
 export function AppDemo(props: { appId: string; appDsl?: any }) {
   return (
     <OpenblocksAppView
-      baseUrl=""
       className="ob-app"
       appId={props.appId}
       appDsl={props.appDsl}
